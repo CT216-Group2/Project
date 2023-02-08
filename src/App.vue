5e46105b3eda3df3aae8be202747c990d07760f2
michael-gill <115096283+michael-gill@users.noreply.github.com>
2023-02-08 12:47:47 +0000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <router-view></router-view>
+  <body>
+    <header id="header">
+    </header>
+    <div id="content-wrap">
+
+    </div>
+    <footer id="footer">
+
+    </footer>
+  </body>
+</template>
+
+<style scoped>
+
+</style>
