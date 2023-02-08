429dd465ecf55bc88e3dd3932c2fb5ee0cac60bd
michael-gill <m.gill15@nuigalway.ie>
2023-02-08 14:34:46 +0000

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
+</script>
+
+<template>
+  <router-view></router-view>
+  <body>
+    <header id="header">
+      <NavBar/>
+    </header>
+    <div id="content-wrap">
+
+    </div>
+    <footer id="footer">
+      <Footer/>
+    </footer>
+  </body>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+ <template>
+  <div>
+  <!-- FOOTER -->
+  <footer class="w-100 p-3 mainColour position-absolute start-0">
+    <div class="container py-1 ">
+      <div class="row gy-4 gx-5">
+        <div class="col-lg-8 col-md-6">
+          <h5 class="h1 text-white mainFont">Accommodate<a class="secondaryColour">Me</a></h5>
+          <p class="small text-white">Where Students meet Houses</p>
+          <p class="small text-secondary mb-0">&copy; Copyrights. All rights reserved. <a class="text-primary" href="#">Group 2</a></p>
+        </div>
+        <div class="col-lg-4 col-md-3">
+          <h5 class="text-white mb-3 mainFont">Quick links</h5>
+          <ul class="list-unstyled text-muted">
+            <li><a class="secondaryColour mainFont" href="#">Home</a></li>
+            <li><a class="secondaryColour mainFont" href="#">About</a></li>
+            <li><a class="secondaryColour mainFont"  href="#">Account</a></li>
+            <li><a class="secondaryColour mainFont" href="#">Sign-Up</a></li>
+          </ul>
+        </div>
+        </div>
+    </div>
+  </footer>
+  </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
