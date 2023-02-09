d280fd4fd1165279cd1539e093f8e89102f3af3e
michael-gill <m.gill15@nuigalway.ie>
2023-02-09 12:02:58 +0000

    Fifth Commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,15 +7,15 @@
         <div class="col-lg-8 col-md-6">
           <h5 class="h1 text-white mainFont">Accommodate<a class="secondaryColour">Me</a></h5>
           <p class="small text-white">Where Students meet Houses</p>
-          <p class="small text-secondary mb-0">&copy; Copyrights. All rights reserved. <a class="text-primary" href="#">Group 2</a></p>
+          <p class="small mainFont black mb-0">&copy; Copyrights. All rights reserved. <a class="mainFont secondaryColour" href="#">Group 2</a></p>
         </div>
         <div class="col-lg-4 col-md-3">
           <h5 class="text-white mb-3 mainFont">Quick links</h5>
           <ul class="list-unstyled text-muted">
-            <li><a class="secondaryColour mainFont" href="#">Home</a></li>
+            <li><a class="secondaryColour mainFont"><router-link to="/" class="secondaryColour">Home</router-link></a></li>
             <li><a class="secondaryColour mainFont" href="#">About</a></li>
-            <li><a class="secondaryColour mainFont"  href="#">Account</a></li>
-            <li><a class="secondaryColour mainFont" href="#">Sign-Up</a></li>
+            <li><a class="secondaryColour mainFont"><router-link to="/StudentAccount" class="secondaryColour">Account</router-link></a></li>
+            <li><a class="secondaryColour mainFont"><router-link to="/StudentSignUp" class="secondaryColour">Sign-Up</router-link></a></li>
           </ul>
         </div>
         </div>
